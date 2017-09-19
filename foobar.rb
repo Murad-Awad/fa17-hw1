75e697f48d988d14272370ff1698c50191d84fb4
Murad Awad <muradawad21@berkeley.edu>
2017-09-18 23:50:05 -0700

    solved q1-q4

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,9 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    a.map!{|element| element=element.to_i}.delete_if{|n|n%2!=0 || n>=10}
+    a=a.uniq
+    a.inject(0){|sum,y|sum+y+2}
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,16 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  d=(a+b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.sort!{|x,y|x<=>y}.map!{|x|x+1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  first_name + " " + last_name
 end
 
 def blockin_time(a)
